abd79fc79729c8811bbece875d1eda225e941ce5
Shawn Thompson <plansmash@me.com>
2015-12-01 23:43:52 -0500

    removed routing added unique function to genre drop down filtered by dropdown

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 //app.js
 var myApp = angular.module('myApp', [
-  'ngRoute',
   'tracksController'
 ]);
 
-myApp.config(['$routeProvider', function($routeProvider){
-  $routeProvider.
-  when('/list', {
-    templateURL: './partials/list.html',
-    controller: 'ListController'
-  }).
-  otherwise({
-    redirectTo: '/list'
-  });
-}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,46 @@
 // controllers.js
 var tracksController = angular.module('tracksController', ['angularUtils.directives.dirPagination'])
 
+tracksController.filter('unique', function () {
+return function ( collection, keyname) {
+var output = [],
+    keys = []
+    found = [];
+
+if (!keyname) {
+
+    angular.forEach(collection, function (row) {
+        var is_found = false;
+        angular.forEach(found, function (foundRow) {
+
+            if (foundRow == row) {
+                is_found = true;                            
+            }
+        });
+
+        if (is_found) { return; }
+        found.push(row);
+        output.push(row);
+
+    });
+}
+else {
+
+    angular.forEach(collection, function (row) {
+        var item = row[keyname];
+        if (item === null || item === undefined) return;
+        if (keys.indexOf(item) === -1) {
+            keys.push(item);
+            output.push(row);
+        }
+    });
+}
+
+return output;
+};
+});
+
+
 tracksController.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('./data/tracks.json').success(function(data) {
       $scope.tracks = data; 
@@ -14,7 +54,7 @@ tracksController.controller('ListController', ['$scope', '$http', function($scop
   }
 }]);
 
-tracksController.controller('CollectionController', ['$scope', function($scope){
+tracksController.controller('DetailsController', ['$scope', function($scope){
   $scope.addedTracks = [];
 }]);
 

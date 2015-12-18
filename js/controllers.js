74c9fd1d65617d43ee1c1cc0ec6f354e8141dea7
Shawn Thompson <plansmash@me.com>
2015-12-17 22:22:34 -0500

    added the push controller funtion, needs tweeking but works

--- js/controllers.js
+++ js/controllers.js
@@ -40,55 +40,25 @@ return output;
 };
 });
 
-
-
-/*
- angular.module('copyExample', [])
-   .controller('ExampleController', ['$scope', function($scope) {
-     $scope.master= {};
-
-     $scope.update = function(user) {
-       // Example with 1 argument
-       $scope.master= angular.copy(user);
-     };
-
-     $scope.reset = function() {
-       // Example with 2 arguments
-       angular.copy($scope.master, $scope.user);
-     };
-
-     $scope.reset();
-   }]);
-*/
-
 tracksController.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('./data/tracks.json').success(function(data) {
       $scope.tracks = data; 
     });
-  $scope.addedTracks = [ //sample data, to be removed before going live
-    { 
-      "ID":2,
-      "Song":"YMCA",
-      "Artist":"Village People",
-      "Genre":"Disco",
-      "TopSong":""
-    }
-  ];
-  $scope.addSong = function(track) {
-    $scope.addedTracks = angular.copy(track);
+  $scope.addedTracks = [];
+
+  $scope.selectSong = function(songIndex, row) {
+    $scope.addedTracks.push({
+            Song : songIndex
+        });
+    $scope.tracks.splice($scope.tracks.indexOf(row),1);
   }
+
   $scope.removeSong = function(row) {
     $scope.addedTracks.splice($scope.addedTracks.indexOf(row),1);
   }
   $scope.clearSelectedSongs = function() {
     $scope.addedTracks = [];
   }
-  $scope.sortType = 'ID'; // set the default sort type
-  $scope.sortReverse = false; // set the default sort order 
-}]);
-
-tracksController.controller('DetailsController', ['$scope', function($scope){
-}]);
-
-tracksController.controller('SortController', ['$scope', function(){
+  $scope.sortType = 'TopSong'; // set the default sort type
+  $scope.sortReverse = true; // set the default sort order 
 }]);

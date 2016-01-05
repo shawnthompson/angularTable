2da8822b420d382541ac9a831e366157f3666b8f
Shawn Thompson <plansmash@me.com>
2016-01-05 16:13:18 -0500

    js cleanup

--- js/controllers.js
+++ js/controllers.js
@@ -2,42 +2,42 @@
 var tracksController = angular.module('tracksController', ['angularUtils.directives.dirPagination'])
 
 tracksController.filter('unique', function () {
-return function ( collection, keyname) {
-var output = [],
-	keys = []
-	found = [];
+	return function ( collection, keyname) {
+		var output = [],
+			keys = []
+			found = [];
 
-if (!keyname) {
+		if (!keyname) {
 
-	angular.forEach(collection, function (row) {
-		var is_found = false;
-		angular.forEach(found, function (foundRow) {
+			angular.forEach(collection, function (row) {
+				var is_found = false;
+				angular.forEach(found, function (foundRow) {
 
-			if (foundRow == row) {
-				is_found = true;
-			}
-		});
+					if (foundRow == row) {
+						is_found = true;
+					}
+				});
 
-		if (is_found) { return; }
-		found.push(row);
-		output.push(row);
-	});
-}
-
-else {
-
-	angular.forEach(collection, function (row) {
-		var item = row[keyname];
-		if (item === null || item === undefined) return;
-		if (keys.indexOf(item) === -1) {
-			keys.push(item);
-			output.push(row);
+				if (is_found) { return; }
+				found.push(row);
+				output.push(row);
+			});
 		}
-	});
-}
 
-return output;
-};
+		else {
+
+			angular.forEach(collection, function (row) {
+				var item = row[keyname];
+				if (item === null || item === undefined) return;
+				if (keys.indexOf(item) === -1) {
+					keys.push(item);
+					output.push(row);
+				}
+			});
+		}
+
+		return output;
+	};
 });
 
 tracksController.controller('ListController', ['$scope', '$http', function($scope, $http) {

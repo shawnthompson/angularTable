6fd1e64af0d2d65b8c14222eec3c72cb0f8cc00b
Shawn Thompson <plansmash@me.com>
2016-04-04 15:22:29 -0400

    added nofollow meta

--- inc/head.php
+++ inc/head.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex">
         <title>SoundTraxx - Ottawa DJ - <?php echo $title; ?></title>
         <!-- CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
